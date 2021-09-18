258b11ce9a24686a027ae82a06a09c0108a1d76b
AlexanderVorvul <80675797+AlexanderVorvul@users.noreply.github.com>
2021-09-18 17:55:30 +0300

    feat: implement chainMaker object

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,35 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  arr: [],
+
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.arr.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  addLink( value ) {
+    this.arr.push("( "+value + " )");
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink( position ) {
+    if (typeof position != "number" || this.getLength() < position || position < 1 ) {
+      this.arr = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    else
+      this.arr.splice( position - 1, 1);
+    return this;
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.arr.reverse();    
+    return this;
   },
+
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const result = this.arr.join("~~");
+    this.arr = [];
+    return result;
   }
 };
