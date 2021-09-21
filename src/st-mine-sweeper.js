e72f480f3c8f58ca8229cecaa1d78dcb7d7146d4
AlexanderVorvul <80675797+AlexanderVorvul@users.noreply.github.com>
2021-09-21 16:17:29 +0300

    feat: implement minesweeper function

--- src/st-mine-sweeper.js
+++ src/st-mine-sweeper.js
@@ -23,7 +23,26 @@ import { NotImplementedError } from '../extensions/index.js';
  *  [1, 1, 1]
  * ]
  */
-export default function minesweeper (/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function minesweeper ( matrix ) {
+  
+  function check(matrix, iPos, jPos){
+    let mines = 0;
+    for(let i = (iPos === 0 ? 0 : iPos-1); i <= (iPos + 1 === matrix.length ? iPos : iPos + 1); i++ )
+      for(let j = (jPos === 0 ? 0 : jPos-1); j <= (jPos + 1 === matrix[iPos].length ? jPos : jPos + 1); j++ )
+        if (matrix[i][j]) mines++;
+    if(matrix[iPos][jPos]) mines--;
+    return mines;
+  }
+
+  let result = new Array();
+
+  for(let i = 0; i < matrix.length; i++ ){
+
+    result.push(new Array());
+
+    for(let j = 0; j < matrix[i].length; j++ )
+
+      result[i].push( check(matrix, i, j) );
+  }  
+  return result;
 }
