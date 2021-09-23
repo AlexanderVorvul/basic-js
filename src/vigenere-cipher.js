d3dd2c8199756e4563acec92ad9eef6fc54c4fb8
AlexanderVorvul <80675797+AlexanderVorvul@users.noreply.github.com>
2021-09-23 12:56:34 +0300

    feat: implement VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,72 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  latinAlphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+  constructor(dir = true) {
+    this.direction = dir;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  getExtKey(STR, key) {    
+    let extendedKey = '';
+    while (extendedKey.length < STR.length) extendedKey += key;    
+    extendedKey = extendedKey.toUpperCase();
+
+    let result = '';
+    for (let i = 0, index = 0; i < STR.length; i++)
+      if ( this.latinAlphabet.indexOf( STR[i] ) === -1 )
+        result += STR[i];
+      else {
+        result += extendedKey[index];
+        index++;
+      }
+    return result;
+  }
+
+  encrypt(str, key) {
+    if (!str || !key) throw new Error(`Incorrect arguments!`);
+
+    const STR = str.toUpperCase();
+    const extendedKey = this.getExtKey(STR, key);
+    const arr = STR.split('');
+
+    let result = arr.map(
+      (element, index) => {        
+        if ( this.latinAlphabet.indexOf( element ) === -1 )
+          return element;
+        else {
+          const strIndex = this.latinAlphabet.indexOf(arr[index]);   // вычисляем индекс нужного символа согласно таблицы Vigenere
+          const keyIndex = this.latinAlphabet.indexOf(extendedKey[index]);   // c = m + k
+          let resultIndex = strIndex + keyIndex;
+          if (resultIndex >= this.latinAlphabet.length) resultIndex -= this.latinAlphabet.length;       
+          return element = this.latinAlphabet[resultIndex];
+        }
+      })
+    if (! this.direction) result = result.reverse();
+    return result.join('');
+  }
+
+  decrypt(str, key) {
+    if (!str || !key) throw new Error(`Incorrect arguments!`);
+    const STR = str.toUpperCase();
+    const extendedKey = this.getExtKey(STR, key);
+    const arr = STR.split('');
+
+    let result = arr.map(
+      (element, index) => {        
+        if ( this.latinAlphabet.indexOf( element ) === -1 ) 
+          return element;
+        else {
+          const strIndex = this.latinAlphabet.indexOf(arr[index]);
+          const keyIndex = this.latinAlphabet.indexOf(extendedKey[index]);
+          let resultIndex = strIndex  + this.latinAlphabet.length - keyIndex;
+          if (resultIndex >= this.latinAlphabet.length) resultIndex -= this.latinAlphabet.length;       
+          return element = this.latinAlphabet[resultIndex];
+        }
+      })
+    if (! this.direction) result = result.reverse();
+    return result.join('');
   }
 }
+
